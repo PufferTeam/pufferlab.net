b539c4b55472c1997d5588768b4f323207b7c29c
JohnButlerThird <112212674+JohnButlerThird@users.noreply.github.com>
2025-03-24 06:17:23 +0000

    Minor fix

--- main.js
+++ main.js
@@ -1,53 +1,51 @@
-var changeModeBT = document.getElementById('changeMode');
-var changeLangSL = document.getElementById('changeLang');
-var toggleMenuBT = document.getElementById('toggleMenu');
+var changeModeBT = document.getElementById("changeMode");
+var changeLangSL = document.getElementById("changeLang");
+var toggleMenuBT = document.getElementById("toggleMenu");
 
-var defaultElements = document.getElementsByClassName('default');
+var defaultElements = document.getElementsByClassName("default");
 for (let i = 0; i < defaultElements.length; i++) {
-    defaultElements[i].classList.add('active')
+    defaultElements[i].classList.add("active");
 }
 
-var navlinkElements = document.getElementsByClassName('navlink');
+var navlinkElements = document.getElementsByClassName("navlink");
 for (let i = 0; i < navlinkElements.length; i++) {
-    let el = navlinkElements[i]
-    el.addEventListener("click", function(event){
-      event.preventDefault()
+    let el = navlinkElements[i];
+    el.addEventListener("click", function (event) {
+        event.preventDefault();
     });
 }
 
 window.addEventListener("popstate", function () {
-    readURL()
-    updateTitle()
+    readURL();
+    updateTitle();
 });
 
-var page = 'home';
+var page = "home";
 
-const validPages = [
-    "error", "", "home", "about"
-]
+const validPages = ["error", "", "home", "about"];
 
-var mainURL = window.location.href.split('/').slice(0, 3).join('/');
+var mainURL = window.location.href.split("/").slice(0, 3).join("/");
 
 var url = window.location.href;
 function readURL() {
     url = window.location.href;
     page = url.slice(mainURL.length + 2);
-    if (page == '') {
-        page = 'home'
+    if (page == "") {
+        page = "home";
     }
 
-    updatePage()
+    updatePage();
 }
 readURL();
 
 function change(id, add, t) {
     let e = document.getElementById(id);
-    if(e != null) {
-        let tag = 'active';
-        if(t != undefined) {
-            tag = t
+    if (e != null) {
+        let tag = "active";
+        if (t != undefined) {
+            tag = t;
         }
-        if(add) {
+        if (add) {
             e.classList.add(tag);
         } else {
             e.classList.remove(tag);
@@ -56,21 +54,21 @@ function change(id, add, t) {
 }
 
 function updatePageClass(page, show) {
-    change(page + 'Page', show);
-    change(page + 'PageMenu', show);
-    change(page, show, 'selected');
-    change(page + 'Menu', show, 'selected');
+    change(page + "Page", show);
+    change(page + "PageMenu", show);
+    change(page, show, "selected");
+    change(page + "Menu", show, "selected");
 }
 
 function updatePage() {
     if (!validPages.includes(page)) {
-        page = 'error'
+        page = "error";
     }
     for (let i = 0; i < validPages.length; i++) {
-        let pageID = validPages[i]
+        let pageID = validPages[i];
         if (pageID != page) {
-            if (pageID == '') {
-                pageID = 'home'
+            if (pageID == "") {
+                pageID = "home";
             }
             updatePageClass(pageID, false);
         }
@@ -79,53 +77,56 @@ function updatePage() {
 }
 
 function changePage(pg) {
-    if(page != pg) {
-        page = pg
-        changeURL()
-        updatePage()
-        updateTitle()
+    if (page != pg) {
+        page = pg;
+        changeURL();
+        updatePage();
+        updateTitle();
     }
 }
 
 function changeURL() {
-    let pageName = '/?' + page
+    let pageName = "/?" + page;
     if (!validPages.includes(page)) {
-        pageName = '/?error'
+        pageName = "/?error";
     }
-    if(page == 'home') {
-        pageName = ''
+    if (page == "home") {
+        pageName = "";
     }
     let pageURL = mainURL + pageName;
-    window.history.pushState({page: page}, "", pageURL);
+    window.history.pushState({ page: page }, "", pageURL);
 }
 
-var mode = localStorage.getItem('savedMode');
+var mode = localStorage.getItem("savedMode");
 if (mode == null) {
-    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        mode = 'mode.dark'
+    if (
+        window.matchMedia &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches
+    ) {
+        mode = "mode.dark";
     } else {
-        mode = 'mode.light'
+        mode = "mode.light";
     }
 }
 
-var lang = localStorage.getItem('savedLang');
+var lang = localStorage.getItem("savedLang");
 if (lang == null) {
-    lang = 'en'
+    lang = "en";
 }
 
-var menu = 'menu.hide';
+var menu = "menu.hide";
 
 function sendRequest(name) {
     let request = new XMLHttpRequest();
-    request.open("GET", name + '.json', false);
-    request.send(null)
+    request.open("GET", name + ".json", false);
+    request.send(null);
     let o = JSON.parse(request.responseText);
-    
-    return o
+
+    return o;
 }
 
-var langFile = sendRequest('lang')
-var svgFile = sendRequest('svg')
+var langFile = sendRequest("lang");
+var svgFile = sendRequest("svg");
 
 const svgStates = ["normal", "hover"];
 function getSvg(key) {
@@ -134,18 +135,18 @@ function getSvg(key) {
         let st = svgStates[i];
         let sKey = svgFile[st][key];
         if (sKey != undefined) {
-            svgA.push(sKey)
-        }       
+            svgA.push(sKey);
+        }
     }
-    svgA.push('</div>');
-    let svgO = svgA.join('');
-    return svgO
+    svgA.push("</div>");
+    let svgO = svgA.join("");
+    return svgO;
 }
 
 function getLang(key) {
-    let fKey = langFile[lang][key]
+    let fKey = langFile[lang][key];
     if (fKey == undefined) {
-        fKey = langFile['en'][key]
+        fKey = langFile["en"][key];
     }
     return fKey;
 }
@@ -154,8 +155,8 @@ function updateIn(cl) {
     let el = document.getElementsByClassName(cl);
     for (let i = 0; i < el.length; i++) {
         let e = el[i];
-        let r = e.getAttribute('name');
-        if (cl == 'svg') {
+        let r = e.getAttribute("name");
+        if (cl == "svg") {
             e.innerHTML = getSvg(r);
         } else {
             e.innerHTML = getLang(r);
@@ -164,8 +165,8 @@ function updateIn(cl) {
 }
 
 function updateMenu() {
-    let el = 'menuPage'
-    if (menu == 'menu.show') {
+    let el = "menuPage";
+    if (menu == "menu.show") {
         change(el, true);
     } else {
         change(el, false);
@@ -176,52 +177,52 @@ updateMenu();
 
 function updateTitle() {
     let tl = getLang(page);
-    let title = 'PufferLab - ' + tl;
+    let title = "PufferLab - " + tl;
     document.title = title;
 }
-updateTitle()
+updateTitle();
 
-updateIn('svg');
+updateIn("svg");
 
 function updateLang() {
     changeLangSL.value = lang;
-    updateTitle()
-    updateIn('lang');
+    updateTitle();
+    updateIn("lang");
 }
-updateLang()
+updateLang();
 
 function updateMode() {
-    let el = 'main'
-    if (mode == 'mode.dark') {
-        change(el, true, 'dark');
+    let el = "main";
+    if (mode == "mode.dark") {
+        change(el, true, "dark");
     } else {
-        change(el, false, 'dark');
+        change(el, false, "dark");
     }
     changeModeBT.innerHTML = getSvg(mode);
 }
-updateMode()
+updateMode();
 
 function toggleMenu() {
-    if (menu == 'menu.hide') {
-        menu = 'menu.show';
+    if (menu == "menu.hide") {
+        menu = "menu.show";
     } else {
-        menu = 'menu.hide'
+        menu = "menu.hide";
     }
     updateMenu();
 }
 
 function changeLang() {
     lang = changeLangSL.value;
-    localStorage.setItem('savedLang', lang)
-    updateLang()
+    localStorage.setItem("savedLang", lang);
+    updateLang();
 }
 
 function changeMode() {
-    if (mode == 'mode.dark') {
-        mode = 'mode.light';
+    if (mode == "mode.dark") {
+        mode = "mode.light";
     } else {
-        mode = 'mode.dark'
+        mode = "mode.dark";
     }
-    localStorage.setItem('savedMode', mode);
-    updateMode()
+    localStorage.setItem("savedMode", mode);
+    updateMode();
 }

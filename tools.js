d7798215e86d56296052eea21da1013f6a428c04
JohnButlerThird <112212674+JohnButlerThird@users.noreply.github.com>
2025-05-19 15:31:56 -0400

    Added new Units

--- tools.js
+++ tools.js
@@ -9,6 +9,7 @@ const unitsConversion = new Map()
 
 let unitTypes = [
     "distance",
+    "area",
     "volume",
     "mass",
     "time",
@@ -22,25 +23,44 @@ if (converterPage == null) {
     converterPage = "distance";
 }
 
-generateMetric('distance', 'meter', 'm')
+generateMetric('distance', 'meter', 'm', 'metric')
 unit('distance', 'inch', 'in', 'imperial', 1, false, 'imperial_us')
 unit('distance', 'foot', 'ft', 'imperial', 12, false, 'imperial_us')
 unit('distance', 'yard', 'yd', 'imperial', 12 * 3, false, 'imperial_us')
 unit('distance', 'mile', 'mi', 'imperial', 12 * 3 * 1760, false, 'imperial_us')
-unit('distance', 'nautical_mile', 'nmi', 'imperial', 1852, false, 'other')
+unit('distance', 'nautical_mile', 'nmi', 'metric', 1852, false, 'other')
 unit('distance', 'astronomical_unit', 'au', 'metric', 149597870700, false, 'other')
 unit('distance', 'light_year', 'ly', 'metric', 9460730472580800, false, 'other')
 
-generateMetric('volume', 'liter', 'L')
-unit('volume', 'cubic_meter', 'm³', 'metric', 1e3, false, 'metric_other')
-unit('volume', 'cubic_decimeter', 'dm³', 'metric', 1, false, 'metric_other')
-unit('volume', 'cubic_centimeter', 'cm³', 'metric', 1e-3, false, 'metric_other')
-unit('volume', 'cubic_millimeter', 'mm³', 'metric', 1e-6, false, 'metric_other')
+unit('area', 'square_kilometer', 'km²', 'metric', Math.pow(1e3, 2), false, 'metric_square')
+unit('area', 'square_hectometer', 'hm²', 'metric', Math.pow(1e2, 2), false, 'metric_square')
+unit('area', 'square_decameter', 'dam²', 'metric', Math.pow(1e1, 2), false, 'metric_square')
+unit('area', 'square_meter', 'm²', 'metric', Math.pow(1, 2), false, 'metric_square', true)
+unit('area', 'square_decimeter', 'dm²', 'metric', Math.pow(1e-1, 2), false, 'metric_square')
+unit('area', 'square_centimeter', 'cm²', 'metric', Math.pow(1e-2, 2), false, 'metric_square')
+unit('area', 'square_millimeter', 'mm²', 'metric', Math.pow(1e-3, 2), false, 'metric_square')
+unit('area', 'square_inch', 'in²', 'imperial', 1, false, 'imperial_us_square')
+unit('area', 'square_foot', 'ft²', 'imperial', Math.pow(12, 2), false, 'imperial_us_square')
+unit('area', 'square_yard', 'yd²', 'imperial', Math.pow(12 * 3, 2), false, 'imperial_us_square')
+unit('area', 'square_mile', 'mi²', 'imperial', Math.pow(12 * 3 * 1760, 2), false, 'imperial_us_square')
+unit('area', 'hectare', 'ha', 'metric', Math.pow(1e2, 2), false, 'other')
+unit('area', 'acre', 'ac', 'imperial', Math.pow(12 * 3 * 1760, 2) / 640, false, 'other')
+
+generateMetric('volume', 'liter', 'L', 'metric')
+unit('volume', 'cubic_kilometer', 'km³', 'metric', Math.pow(1e4, 3), false, 'metric_cubic')
+unit('volume', 'cubic_hectometer', 'hm³', 'metric', Math.pow(1e3, 3), false, 'metric_cubic')
+unit('volume', 'cubic_decameter', 'dam³', 'metric', Math.pow(1e2, 3), false, 'metric_cubic')
+unit('volume', 'cubic_meter', 'm³', 'metric', Math.pow(1e1, 3), false, 'metric_cubic')
+unit('volume', 'cubic_decimeter', 'dm³', 'metric', 1, false, 'metric_cubic')
+unit('volume', 'cubic_centimeter', 'cm³', 'metric', Math.pow(1e-1, 3), false, 'metric_cubic')
+unit('volume', 'cubic_millimeter', 'mm³', 'metric', Math.pow(1e-2, 3), false, 'metric_cubic')
 unit('volume', 'metric_cup', 'cup', 'metric', 0.25, false, 'metric_other')
 unit('volume', 'metric_tablespoon', 'tbsp', 'metric', 0.015, false, 'metric_other')
 unit('volume', 'metric_teaspoon', 'tsp', 'metric', 0.005, false, 'metric_other')
-unit('volume', 'cubic_inch', 'in³', 'imperial', 1, false, 'imperial_us')
-unit('volume', 'cubic_foot', 'ft³', 'imperial', 12 * 12 * 12, false, 'imperial_us')
+unit('volume', 'cubic_inch', 'in³', 'imperial', 1, false, 'imperial_us_cubic')
+unit('volume', 'cubic_foot', 'ft³', 'imperial', Math.pow(12, 3), false, 'imperial_us_cubic')
+unit('volume', 'cubic_yard', 'yd³', 'imperial', Math.pow(12 * 3, 3), false, 'imperial_us_cubic')
+unit('volume', 'cubic_mile', 'mi³', 'imperial', Math.pow(12 * 3 * 1760, 3), false, 'imperial_us_cubic')
 let us_gal = 231
 unit('volume', 'us_gallon', 'gal (US)', 'imperial', us_gal, false, 'us')
 unit('volume', 'us_quart', 'qt (US)', 'imperial', us_gal / 4, false, 'us')
@@ -60,13 +80,13 @@ unit('volume', 'imp_fluid_dram', 'fl dr (imp)', 'imperial', imp_gal / 160 / 8, f
 unit('volume', 'imp_tablespoon', 'tbsp (imp)', 'imperial', imp_gal / 160 / 2, false, 'imperial')
 unit('volume', 'imp_teaspoon', 'tsp (imp)', 'imperial', imp_gal / 160 / 2 / 4, false, 'imperial')
 
-generateMetric('mass', 'gram', 'g')
-unit('mass', 'tonne', 't', 'metric', 1e6, false, 'metric')
+generateMetric('mass', 'gram', 'g', 'metric')
+unit('mass', 'tonne', 't', 'metric', 1e6, false, 'metric_other')
 unit('mass', 'ounce', 'oz', 'imperial', 1, false, 'imperial_us')
 unit('mass', 'pound', 'lb', 'imperial', 16, false, 'imperial_us')
-unit('mass', 'stone', 'st', 'imperial', 16 * 14, false, 'imperial')
-unit('mass', 'long_ton', 'LT (UK)', 'imperial', 16 * 2240, false, 'imperial')
-unit('mass', 'short_ton', 'st (US)', 'imperial', 16 * 2000, false, 'us')
+unit('mass', 'stone', 'st', 'imperial', 16 * 14, false, 'imperial_us')
+unit('mass', 'long_ton', 'LT (UK)', 'imperial', 16 * 2240, false, 'imperial_us_other')
+unit('mass', 'short_ton', 'st (US)', 'imperial', 16 * 2000, false, 'imperial_us_other')
 
 unit('time', 'millennium', '', '', 60 * 60 * 24 * 365 * 1e3)
 unit('time', 'century', '', '', 60 * 60 * 24 * 365 * 1e2)
@@ -103,8 +123,7 @@ unit('data', 'gigabyte', 'GB', 'data', Math.pow(1000, 3), false, 'metric')
 unit('data', 'terabyte', 'TB', 'data', Math.pow(1000, 4), false, 'metric')
 unit('data', 'petabyte', 'PB', 'data', Math.pow(1000, 5), false, 'metric')
 
-function generateMetric(type, name, symbol) {
-    let m = 'metric';
+function generateMetric(type, name, symbol, m) {
     unit(type, 'tera' + name, 'T' + symbol, m, 1e12, false, m)
     unit(type, 'giga' + name, 'G' + symbol, m, 1e9, false, m)
     unit(type, 'mega' + name, 'M' + symbol, m, 1e6, false, m)
@@ -121,13 +140,15 @@ function generateMetric(type, name, symbol) {
 }
 
 unitCategories('distance', ['metric', 'imperial_us', 'other'])
-unitCategories('volume', ['metric', 'metric_other', 'imperial_us', 'us', 'imperial'])
-unitCategories('mass', ['metric', 'imperial_us', 'us', 'imperial'])
+unitCategories('area', ['metric_square', 'imperial_us_square', 'other'])
+unitCategories('volume', ['metric', 'metric_cubic', 'metric_other', 'imperial_us_cubic', 'us', 'imperial'])
+unitCategories('mass', ['metric', 'metric_other', 'imperial_us', 'imperial_us_other'])
 unitCategories('data', ['default', 'metric', 'ram'])
 
 unitConversion('distance', 'imperial', 'metric', 0.0254, false)
 unitConversion('mass', 'imperial', 'metric', 28.349523125, false)
-unitConversion('volume', 'imperial', 'metric', 61.0237440947, true)
+unitConversion('area', 'imperial', 'metric', Math.pow(0.0254, 2), false)
+unitConversion('volume', 'imperial', 'metric', Math.pow(0.0254, 3) * 1e3, false)
 unitConversion('temperature', 'imperial', 'international', 9 / 5, true)
 
 function unit(type, name, symbol, system, ratio, sign, category, defaultUnit) {
@@ -168,7 +189,7 @@ unitTypes.forEach((e) => {
     PageConverterContent.push(`<div id="Page${eu}Converter" class="converter page">`)
     converterRows.forEach((r) => {
         let ru = main.capitalize(r)
-        PageConverterContent.push(`<div class="converter row ${r}"><label for="${e}Converter${ru}"><select name="${e}Converter${ru}SL" id="${e}Converter${ru}SL" class="trigger select ${e} in" onchange="changeUnit('${e}Converter', '${ru}', false)"></select>:</label><input type="number" id="${e}Converter${ru}" class="trigger text in" name="${e}Converter${ru}" onkeyup="changeUnit('${e}Converter', '${ru}', true)" required minlength="1" /></div>`);
+        PageConverterContent.push(`<div class="converter row ${r}"><label for="${e}Converter${ru}"><select name="${e}Converter${ru}SL" id="${e}Converter${ru}SL" class="trigger select ${e} in" onchange="changeUnit('${e}', '${ru}', false)"></select>:</label><input type="number" id="${e}Converter${ru}" class="trigger text in" name="${e}Converter${ru}" onkeyup="changeUnit('${e}', '${ru}', true)" required minlength="1" /></div>`);
     })
     PageConverterContent.push(`</div>`)
 })
@@ -270,7 +291,7 @@ function convertUnit(unitFrom, unitTo, value) {
     }
     unitValue = invertEquation(false, unitConverted, unitToMap.ratio, unitToMap.sign);
 
-    return parseFloat(unitValue.toFixed(10));
+    return parseFloat(unitValue.toFixed(15));
 }
 
 function changeConverterPage() {
@@ -284,10 +305,11 @@ window.changeConverterPage = changeConverterPage;
 
 let lastUsedText = ''
 function changeUnit(id1, type, isText) {
+    id1 = id1 + "Converter"
     if (isText) {
         lastUsedText = type;
     }
-    if(lastUsedText !== type && lastUsedText !== '') {
+    if (lastUsedText !== type && lastUsedText !== '') {
         type = lastUsedText
     }
 

0fa72637ab827b5abfda5b70d9878e62cd75e88a
JohnButlerThird <112212674+JohnButlerThird@users.noreply.github.com>
2025-09-12 23:01:37 -0400

    revert the init file

--- init.js
+++
@@ -1,7 +0,0 @@
-var mode = localStorage.getItem("savedMode");
-let e = document.getElementById("main");
-if (mode == "mode.dark") {
-    e.style.backgroundColor = "#0d0d0d";
-} else {
-    e.style.backgroundColor = "#ffffff";
-}
